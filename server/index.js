a4f054fb2ed1b63f80e5795ebe5a1cb9870cc884
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2025-11-15 15:55:46 +0200

    adding packages

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const connect = () => {
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("Connnected to the DB")
+    })
+    .catch((err) => {
+        throw err;
+    })
+}
 
 const app = express()
 app.listen(8800, () => {
+    connect()
     console.log("Connected!")
 })
-
-// test
